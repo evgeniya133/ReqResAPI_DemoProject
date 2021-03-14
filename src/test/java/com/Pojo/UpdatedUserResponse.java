fe3cfc65576b74a9d4d3fd59ec4853956d3e7a07
evgeniya133 <euu1992@gmail.com>
2021-03-13 21:46:40 -0500

    added UpdatedUserResponse

---
+++ src/test/java/com/Pojo/UpdatedUserResponse.java
@@ -0,0 +1,31 @@
+package com.Pojo;
+
+import java.util.Objects;
+
+public class UpdatedUserResponse extends UpdatedUser {
+    private String createdAt;
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdatedUserResponse{" +
+                "createdAt='" + createdAt + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UpdatedUserResponse)) return false;
+        if (!super.equals(o)) return false;
+        UpdatedUserResponse that = (UpdatedUserResponse) o;
+        return createdAt.equals(that.createdAt);
+    }
+}
